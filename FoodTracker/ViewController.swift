9132b10f718b726e47ae60ec76bbfb36166b33a3
Matthew Reeds <mattlreeds@gmail.com>
2020-08-20 00:31:10 -0500

    changes

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Matthew Reeds on 8/19/20.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
