31008bcace4faf6412a044aca5e6e0b79b093d4a
Matthew Reeds <mattlreeds@gmail.com>
2020-08-20 23:41:06 -0500

    actions and outlets created

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextFField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
-
+//MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
